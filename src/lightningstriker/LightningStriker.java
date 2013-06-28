224874751802bb43282cd4a976a02a7c6d12e4fc
Amir <Amir@192.168.2.6>
2013-06-28 17:24:23 -0400

    First Commit

---
+++ src/lightningstriker/LightningStriker.java
@@ -0,0 +1,42 @@
+package lightningstriker;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Material;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LightningStriker extends JavaPlugin {
+
+    public String prefix;
+    public final static Logger logger = Logger.getLogger("minecraft");
+    
+    @Override
+    public void onDisable() {
+        PluginDescriptionFile pdf = this.getDescription();
+        logger.log(Level.INFO, "{0}, version {1} coded by {2} has been Disabled!", new Object[]{pdf.getName(), pdf.getVersion(), pdf.getAuthors()});
+    }
+    
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        PluginDescriptionFile pdf = this.getDescription();
+        logger.log(Level.INFO, "{0}, version {1} coded by {2} has been Enabled!", new Object[]{pdf.getName(), pdf.getVersion(), pdf.getAuthors()});
+    }
+    
+    public void onPlayerInteractBlock(PlayerInteractEvent e) {
+        if (e.getPlayer().getItemInHand().getTypeId() == Material.DIAMOND_AXE.getId()) {
+            if (e.getClickedBlock().getType() != Material.AIR) {
+                if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                    if (e.getPlayer().getLevel() == 1) {
+                        int exp = e.getPlayer().getLevel();
+                        e.getPlayer().setLevel(exp--);
+                        e.getPlayer().getWorld().strikeLightning(e.getClickedBlock().getLocation());
+                    }
+                }
+            }
+        }
+    }
+}
