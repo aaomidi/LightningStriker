6ccaeb0a8cd4a75eeb45ec7a4ec446a4ce121981
Amir <Amir@192.168.2.6>
2013-06-28 17:54:55 -0400

    Done :D

--- src/dev/nationcraft/org/LightningStriker/LightningStriker.java
+++ src/dev/nationcraft/org/LightningStriker/LightningStriker.java
@@ -5,14 +5,15 @@ import java.util.logging.Logger;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class LightningStriker extends JavaPlugin {
+public class LightningStriker extends JavaPlugin implements Listener {
 
-    public String prefix;
     public final static Logger logger = Logger.getLogger("minecraft");
 
     @Override
@@ -23,9 +24,9 @@ public class LightningStriker extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
         PluginDescriptionFile pdf = this.getDescription();
-        getServer().getPluginManager().registerEvents(this, this);
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
         logger.log(Level.INFO, "{0}, version {1} coded by {2} has been Enabled!", new Object[]{pdf.getName(), pdf.getVersion(), pdf.getAuthors()});
     }
 
@@ -33,13 +34,11 @@ public class LightningStriker extends JavaPlugin {
     public void onPlayerInteractBlock(PlayerInteractEvent e) {
         if (e.getPlayer().getItemInHand().getTypeId() == Material.DIAMOND_AXE.getId()) {
             if (e.getPlayer().getItemInHand().getEnchantmentLevel(Enchantment.DAMAGE_ALL) == 2) {
-                if (e.getClickedBlock().getType() != Material.AIR) {
-                    if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                        if (e.getPlayer().getLevel() >= 1) {
-                            int exp = e.getPlayer().getLevel();
-                            e.getPlayer().setLevel(exp--);
-                            e.getPlayer().getWorld().strikeLightning(e.getClickedBlock().getLocation());
-                        }
+                if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                    if (e.getPlayer().getLevel() >= 1) {
+                        int exp = e.getPlayer().getLevel();
+                        e.getPlayer().setLevel(exp-1);
+                        e.getPlayer().getWorld().strikeLightning(e.getClickedBlock().getLocation());
                     }
                 }
             }
